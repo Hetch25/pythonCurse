f0ca09cc6a98990c38394e7d2b53f333095c360b
Hetch25 <nosli_reieva@hotmail.com>
2021-11-11 22:55:42 -0600

    what I made in day 1

---
+++ Clase 2/e1.py
@@ -0,0 +1,27 @@
+#int
+a = 2
+#float
+b = 2.3
+#string
+c = "Hello world"
+#Boolean
+d = True
+e = False
+
+#print (c)
+
+x=1
+x=x+1
+#print (x)
+
+#Type data
+#city = str(input("Type your city you grew up:")
+#print(type(city)))
+
+num1 = int(input("Type your first number"))
+num2 = int(input("Type a second number"))
+sum = num1 + num2
+print(f"Sum = {sum}")
+
+
+
